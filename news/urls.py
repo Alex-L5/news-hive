813030afc8070d8e9529951896636a7ba41bd64a
Alex-L5 <almenkiti@aol.com>
2026-02-15 18:33:01 +0000

    Update app settings

--- news/urls.py
+++ news/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
 # urlpatterns = [
     # path('', views.index, name='index'),
     # path('post/<int:post_id>/', views.post_detail, name='post_detail'),
+    # path('upvote/<int:post_id>/', views.upvote, name='upvote'),
 # ]
 # Main djangonews/urls.py
 # from django.contrib import admin
